c61d27b7042d949093262eadeef5c2c014628e21
SG9357 <141806460+SG9357@users.noreply.github.com>
2024-07-24 01:35:53 +0530

    Update DOC2PDF.py

--- DOC2PDF.py
+++ DOC2PDF.py
@@ -7,9 +7,9 @@ import tempfile
 import time
 
 st.set_page_config(
-    page_title='DOC2PDF', 
-    layout="centered", 
-    initial_sidebar_state="auto", 
+    page_title='DOC2PDF',
+    layout="centered",
+    initial_sidebar_state="auto",
     menu_items=None
 )
 
@@ -30,7 +30,7 @@ def save_uploadedfile(uploadedfile):
 
 def convert_to_pdf_stream(docx_file):
     output_pdf = os.path.splitext(docx_file)[0] + '.pdf'
-    subprocess.run(['libreoffice', '--headless', '--convert-to', 'pdf', docx_file, '--outdir', os.path.dirname(output_pdf)], check=True)
+    subprocess.run(['unoconv', '-f', 'pdf', docx_file], check=True)
     
     with open(output_pdf, "rb") as pdf_file:
         pdf_stream = io.BytesIO(pdf_file.read())
@@ -74,47 +74,37 @@ def main():
 
     st.markdown("""
         <style>
-        
         [class="main css-uf99v8 ea3mdgi5"],[class="css-qg4qf ezrtsby2"]{
             background-color: rgb(133, 157, 216);
         }
-        
         .stRadio > div {
             background-color: #4D898C;
             padding: 10px;
             border-radius: 10px;
             box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;
         }
-        
         .stRadio > label {
             font-size:50%;
         }
-        
         button[kind="primary"] {
         background-color:#C1C8E4;
         font-weight:bold;
         box-shadow: rgba(240, 46, 170, 0.4) -5px 5px, rgba(240, 46, 170, 0.3) -10px 10px, rgba(240, 46, 170, 0.2) -15px 15px, rgba(240, 46, 170, 0.1) -20px 20px, rgba(240, 46, 170, 0.05) -25px 25px;
-        
         }
-        
         button[kind="primary"]:hover {
         background-color: #DEB89B;
         }
-    
         [class="css-mrc9ir ef3psqc11"] {
             background-color: #7FAD9C;
             font-weight:bold;
             box-shadow: rgba(240, 46, 170, 0.4) 5px 5px, rgba(240, 46, 170, 0.3) 10px 10px, rgba(240, 46, 170, 0.2) 15px 15px, rgba(240, 46, 170, 0.1) 20px 20px, rgba(240, 46, 170, 0.05) 25px 25px;
         }
-        
         [class="css-mrc9ir ef3psqc11"]:hover {
             background-color: #DEB89B;
         }
-        
         [class="st-bo st-bp st-br st-bq st-bt st-dy st-b8 st-e1 st-e0"]{
             background-color: #4D898C;
         }
-        
         </style>
     """, unsafe_allow_html=True)
 
